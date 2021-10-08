fa303c5277aaecdc21c4456fb42f4bcf3681c676
Manuel Jesús Romero Mateos <manu4.2012@gmail.com>
2021-10-08 22:26:34 +0200

    Añadido archivo vacío ListaEnlazada.h

---
+++ ListaEnlazada.h
@@ -0,0 +1,14 @@
+//
+// Created by Manuel Jesús Romero Mateos on 8/10/21.
+//
+
+#ifndef EEDD_LISTAENLAZADA_H
+#define EEDD_LISTAENLAZADA_H
+
+
+class ListaEnlazada {
+
+};
+
+
+#endif //EEDD_LISTAENLAZADA_H
