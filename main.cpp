87ee10c3ec503558f80eda6450efde9e3f1a3c92
Manuel Jesús Romero Mateos <manu4.2012@gmail.com>
2021-11-21 20:49:40 +0100

    trabajado 21/11/2021 (lo siento mucho, de verdad) II

--- main.cpp
+++ main.cpp
@@ -24,6 +24,35 @@ int main(int argc, char* argv[]) {
         Usuario us("a","b","0",fe,ub);
         TarjetaVacunacion tar(&us);
         int dos = tar.dosisPorAdministrar();
+
+        cout << "==== Programa de entrenamiento 1 ====" << endl;
+
+        cout << "==== Programa de entrenamiento 2 ====" << endl;
+
+        cout << "*** Instanciación de objetos" << endl;
+
+        cout << "*** Generación de tarjetas de vacunación" << endl;
+
+        cout << "*** Factor de carga de la tabla" << endl;
+
+        cout << "*** Eliminar tarjetas de vacunación de NSS acabados en 0" << endl;
+
+        cout << "*** Número de colisiones máximo producido" << endl;
+
+        cout << "*** Primera dosis a NSS pares" << endl;
+
+        cout << "*** Segunda dosis entre 20 y 50 años" << endl;
+
+        cout << "*** NSS acabados en 2 y 6 sin pauta completa" << endl;
+
+        cout << "*** Reañadir tarjetas de vacunación de NSS acabados en 30" << endl;
+
+        cout << "*** Redispersión" << endl;
+
+        cout << "*** Mostrar usuarios sin pauta recomendada" << endl;
+
+        cout << "*** Diferencia de tiempos" << endl;
+        
         return 0;
     } catch (std::exception &e) {
         cout << e.what();
